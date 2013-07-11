d628d19765241df1e5d0613da3638e419c81c166
Alex Dobson <alex.dobson@live.com>
2013-07-10 19:18:35 -0600

    Added example echo statements for an actual website to password-generator.php in the comments so users could user them as an example of how to add the script to their website.

--- password-generator.php
+++ password-generator.php
@@ -2,7 +2,7 @@
 	// Program for creating a randomly generated password from two actual English words
 	// so it is slightly easier to remember.
 	
-	$lines = file("word.list");
+	$lines = file("word.list"); // Put in location on server ect...
 	$size = sizeof($lines);
 	
 	$rand_one = rand(0, $size);
@@ -13,27 +13,78 @@
 	
 	$password = $word_one . $word_two;
 	
+	$valid_characters = array();
+	// Cover all my vowels
 	if (stristr($password, "A")) {
-		echo "Found 'A'" . "\n"; 
+		array_push($valid_characters, "A");
+	} 
+	if (stristr($password, "E")) {
+		array_push($valid_characters, "E");
+	}
+	if (stristr($password, "I")) {
+		array_push($valid_characters, "I");
+	}
+	if (stristr($password, "O")) {
+		array_push($valid_characters, "O");
+	}
+	if (stristr($password, "U")) {
+		array_push($valid_characters, "U");
+	}
+	// Get a couple of consonants
+	if (stristr($password, "G")) {
+		array_push($valid_characters, "G");
+	}
+	if (stristr($password, "S")) {
+		array_push($valid_characters, "S");
+	}
+	if (stristr($password, "T")) {
+		array_push($valid_characters, "T");
+	}
+	shuffle($valid_characters);
+	$changing_character = $valid_characters[0];
+	
+	if ($changing_character == "A") {
 		$password = str_replace("A", "4", $password);
 		$password = str_replace("a", "4", $password); 
-	} else if (stristr($password, "E")) {
-		echo "Found 'E'" . "\n";
+	} 
+	else if ($changing_character == "E") {
 		$password = str_replace("E", "3", $password);
-		$password = str_replace("e", "3", $password);
-	} else if (stristr($password, "G")) {
-		echo "Found 'G'" . "\n";
+		$password = str_replace("e", "3", $password); 
+	} 
+	else if ($changing_character == "I") {
+		$password = str_replace("I", "1", $password);
+		$password = str_replace("i", "1", $password); 
+	} 
+	else if ($changing_character == "O") {
+		$password = str_replace("O", "0", $password);
+		$password = str_replace("o", "0", $password); 
+	} 
+	else if ($changing_character == "U") {
+		$password = str_replace("U", "Ü", $password);
+		$password = str_replace("u", "ü", $password); 
+	} 
+	else if ($changing_character == "G") {
 		$password = str_replace("G", "9", $password);
-		$password = str_replace("g", "9", $password);
-	} else if (stristr($password, "L")) {
-		echo "Found 'L'" . "\n";
-		$password = str_replace("L", "1", $password);
-		$password = str_replace("l", "1", $password);
-	} else if (stristr($password, "T")) {
-		echo "Found 'T'" . "\n";
+		$password = str_replace("g", "9", $password); 
+	} 
+	else if ($changing_character == "S") {
+		$password = str_replace("S", "\$", $password);
+		$password = str_replace("s", "\$", $password); 
+	} 
+	else if ($changing_character == "T") {
 		$password = str_replace("T", "7", $password);
-		$password = str_replace("t", "7", $password);
+		$password = str_replace("t", "7", $password); 
 	}
 	
+	// Be sure to edit this code to display more eliquently on your webpage, example below:
+	/*
+		echo "<h2>";
+		echo "Your password is: ";
+		echo "</h2>";
+	
+		echo "<h3 style=\"text-align: center; border: 1px solid black; padding: 3px 3px 3px 3px;\">";
+		echo $password;
+		echo "</h3>";
+	*/
 	echo $password . "\n";
 ?>
