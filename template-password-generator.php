589fee0bf54740b440ea3a4067b7aa8386d075d7
Alex Dobson <alex.dobson@live.com>
2014-04-24 12:18:02 -0600

    Changed Template File so that all CSS is contained within the file. Easier for use on other sites.

--- template-password-generator.php
+++ template-password-generator.php
@@ -1,14 +1,33 @@
 <?php /* Template Name: PHP Password Generator */ ?>
-<?php get_header(); ?>
+<?php
+  get_header();
+  do_action('dobsondev_before_content');
+?>
+<script type="text/javascript" src="/wp-content/themes/dobsondev-child/password-generator.js"></script>
+<style type="text/css">
+  #password-location-div {
+    text-align: center;
+    border: 1px solid black;
+    padding: 3px 3px 3px 3px;
+  }
 
-<div class="page-body">
+  #password-input {
+     text-align: center;
+     font-size: 225%;
+     margin: 0 auto;
+  }
+</style>
+
 <h2> Your password is: </h2>
 
 <div id="loading" class="spin-1"> </div>
-<h3 id="password-generator-h3" hidden> <div id="password-location"> </div> </h3>
+<div id="password-location-div"> <input type="text" id="password-input" size="30" /> </div>
 <h4 style="text-align: center; margin-top: 30px;"> Readablility </h4>
-<div style="margin-left: 38%; margin-bottom: 30px;">More Readable <input type="range" name="readability" id="readability" min="1" max="3"> More Secure </div>
+<div style="text-align: center; margin-bottom: 30px;">More Readable <input type="range" name="readability" id="readability" min="1" max="3"> More Secure </div>
 <div class="blue-button" id="new-password-button" onClick="getPassword();"> New Password </div>
-</div>
 
-<?php get_footer(); ?>
+<?php
+  do_action('dobsondev_menu');
+  do_action('dobsondev_after_content');
+  get_footer();
+?>
